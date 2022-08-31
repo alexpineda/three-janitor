aaf7878a5c546fb89378cd1643c18ef61ed08e27
Ricky Alejandro <586716+alexpineda@users.noreply.github.com>
2022-08-31 05:42:14 -0400

    add connectAudio and jsdocs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Version](https://badgen.net/npm/v/three-janitor?color=green)](https://www.npmjs.com/package/three-janitor)
 
-A simple, zero-dependency library to help track your objects and dispose of them, primarily for vanilla [three.js](https://threejs.org/)
+A janitor is a utility class that can be used to clean up resources that are no longer needed. Simple with zero-dependencies, primarily for vanilla [three.js](https://threejs.org/)
 
 ## Installation
 
@@ -15,17 +15,21 @@ npm install three three-janitor
 ## Usage
 
 ```ts
+import { Janitor } from "three-janitor"
+
 const janitor = new Janitor();
 
-// add an Object3D
+// track an Object3D
 const myObject = new THREE.Mesh(...);
 janitor.add(myObject);
 
-// or just on a single line
+// or simply on a single line
 const myOtherObject = janitor.add(new THREE.Mesh(...));
 
 // add callbacks
-janitor.add(() => { /* my clean up function*/})
+const music = new PretendMusic();
+music.play();
+janitor.add(() => music.stop())
 
 // add another janitor to the top level janitor? why not!
 janitor.add(someOtherFunctionThatReturnsAJanitor());
@@ -59,6 +63,8 @@ janitor.addEventListener(window, "click", () => alert(`I'm good to go`));
 
 // interval
 janitor.setInterval(() => console.log("interval!"), 5000);
+
+janitor.connectAudio(sourceAudioNode, gainAudioNode, audioContext.destination);
 
 ```
 ### Contributions welcome.
--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,48 @@ interface Disposable {
 type Callback = () => void;
 type SupportedJanitorTypes = Object3D | Disposable | Callback;
 
+/**
+ * A janitor is a utility class that can be used to clean up resources that are no longer needed.
+ */
 export class Janitor {
     #objects = new Set<Object3D>();
     #disposable = new Set<Disposable>();
     #callbacks = new Set<Callback>();
 
+    /**
+     * @param obj A compatible disposable object to track. Same as janitor.add();
+     */
     constructor(dispose?: SupportedJanitorTypes) {
         if (dispose) {
             this.add(dispose);
         }
     }
 
+    /**
+     * 
+     * @param element The dispatching element
+     * @param event The name of the event
+     * @param callback The event listener
+     * @param options Any event listener options
+     */
     addEventListener(element: { addEventListener: Function, removeEventListener: Function }, event: string, callback: Function, options?: AddEventListenerOptions) {
         element.addEventListener(event, callback, options);
         this.add(() => element.removeEventListener(event, callback));
-        return this;
     }
 
+    /**
+     * Helper for adding and removing setInterval
+     * @param callback A callback function
+     * @param interval ms interval to call the callback
+     */
     setInterval(callback: Callback, interval: number) {
         const _i = setInterval(callback, interval);
         this.add(() => clearInterval(_i));
     }
 
+    /**
+     * @param obj A compatible disposable object to track
+     */
     add<T extends SupportedJanitorTypes>(obj: T): T {
         if (obj instanceof Object3D) {
             this.object3d(obj);
@@ -42,18 +62,45 @@ export class Janitor {
         return obj;
     }
 
+    /**
+     * Helper for connecting and disconnecting AudioNodes.
+     * @param args An array of AudioNodes to connect and later disconnect
+     */
+    connectAudio(...args: AudioNode[]) {
+        for (let i = 0; i < args.length - 2; i++) {
+            args[i].connect(args[i + 1])
+        }
+        this.add(() => {
+            for (let i = 0; i < args.length - 2; i++) {
+                args[i].disconnect(args[i + 1])
+            }
+        });
+    }
+
+    /**
+     * @param callback A callback function
+     */
     callback(callback: Callback) {
         this.#callbacks.add(callback);
     }
 
+    /**
+     * @param obj A disposable object
+     */
     disposable(obj: Disposable) {
         this.#disposable.add(obj);
     }
 
+    /**
+     * @param obj A three.js object
+     */
     object3d(obj: Object3D) {
         this.#objects.add(obj);
     }
 
+    /**
+     * Dispose all objects and callbacks
+     */
     dispose() {
 
         if (this.#objects.size) {
